fc51cbdaae34efa2a7ffef6fe88ae490269acee7
yikwing <hubeieasy@gmail.com>
2023-03-01 09:52:11 +0800

    feat: 自定义topbar

---
+++ app/src/main/java/com/example/studydemo/ui/components/AppBar.kt
@@ -0,0 +1,53 @@
+package com.example.studydemo.ui.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.statusBars
+import androidx.compose.foundation.layout.windowInsetsTopHeight
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun AppBar(content: @Composable () -> Unit) {
+    val barHeight = 56.dp
+
+    Column(
+        modifier = Modifier.background(
+            Brush.linearGradient(
+                listOf(Color(0xFF5EFCE8), Color(0xFF736EFE)),
+            ),
+        ),
+    ) {
+        Spacer(
+            modifier = Modifier.windowInsetsTopHeight(WindowInsets.statusBars).fillMaxWidth(),
+        )
+        Row(
+            modifier = Modifier.height(barHeight).fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center,
+        ) {
+            content()
+        }
+    }
+}
+
+@Preview
+@Composable
+fun AppBarPreview() {
+    AppBar {
+        Text("我来组成头部", color = Color.White, fontSize = 30.sp)
+    }
+}
--- app/src/main/java/com/example/studydemo/ui/screens/MineScreen.kt
+++ app/src/main/java/com/example/studydemo/ui/screens/MineScreen.kt
@@ -1,10 +1,18 @@
 package com.example.studydemo.ui.screens
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.sp
+import com.example.studydemo.ui.components.AppBar
 
 @Composable
 fun MineScreen() {
-    Text(text = "我的页", fontSize = 30.sp)
+    Column {
+        AppBar {
+            Text("我来组成头部", color = Color.White, fontSize = 20.sp)
+        }
+        Text(text = "我的页", fontSize = 30.sp)
+    }
 }
