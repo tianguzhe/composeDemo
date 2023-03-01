5550abe25ae0c1868758cca27f1d43e33f4f91e9
yikwing <hubeieasy@gmail.com>
2023-03-01 13:26:59 +0800

    feat: 统一版本

--- app/src/main/java/com/example/studydemo/ui/screens/StudyScreen.kt
+++ app/src/main/java/com/example/studydemo/ui/screens/StudyScreen.kt
@@ -1,13 +1,20 @@
 package com.example.studydemo.ui.screens
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
+import com.example.studydemo.ui.components.AppBar
 
 @Composable
 fun StudyScreen() {
-    Text(text = "学习页", fontSize = 30.sp)
+    Column {
+        AppBar {
+            Text(text = "学习页", fontSize = 30.sp)
+        }
+//        Image(painter = , contentDescription = null)
+    }
 }
 
 @Preview(showBackground = true)
